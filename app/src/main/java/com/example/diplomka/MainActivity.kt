0dce108288124ee5cafcb664fec107a22992dc89
AziTimur <azik00kgz@gmail.com>
2022-02-09 20:19:56 +0600

    add menu and navigation

--- app/src/main/java/com/example/diplomka/MainActivity.kt
+++ app/src/main/java/com/example/diplomka/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.diplomka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.diplomka.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private val binding: ActivityMainBinding by viewBinding()
+    private lateinit var navController: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_main)
+       initNavigation()
     }
+
+    private fun initNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+    }
+
+
 }
 
